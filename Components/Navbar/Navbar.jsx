ccdf14c2a9a66ebf9f1a77702214a2ac9d8737a1
Vamika Arya <vamika.arya2005@gmail.com>
2025-07-07 14:51:08 +0530

    Modifications in Navbar

--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import {React,useState} from 'react'
+
 import './Navbar.css'
 // import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
+  const [menu,setMenu] = useState("home");
   return (
     <div className='navbar'>
         {/* <img src={logo} alt="" /> */}
         <ul className="nav-menu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Projects</li>
-            <li>Achivements</li>
-            <li>Portfolio</li>
-            <li>Contacts</li>
+            <li onClick={()=>{setMenu("home")}} style={menu=="home"?{textDecoration : "underline" , transition: "0.5s", transform:"scale(1.55"}:{}}>Home</li>
+            <li onClick={()=>{setMenu("about-me")}} style={menu=="about-me"?{textDecoration : "underline", transition: "0.5s", transform:"scale(1.5)"}:{}}>About Me</li>
+            <li onClick={()=>{setMenu("projects")}}  style={menu=="projects"?{textDecoration : "underline", transition: "0.5s", transform:"scale(1.5)"}:{}}>Projects</li>
+            <li onClick={()=>{setMenu("achievements")}} style={menu=="achievements"?{textDecoration : "underline", transition: "0.5s", transform:"scale(1.5)"}:{}}>Achivements</li>
+            <li onClick={()=>{setMenu("portfolio")}} style={menu=="portfolio"?{textDecoration : "underline", transition: "0.5s", transform:"scale(1.5)"}:{}}>Portfolio</li>
+            <li onClick={()=>{setMenu("contacts")}} style={menu=="contacts"?{textDecoration : "underline", transition: "0.5s", transform:"scale(1.5)"}:{}}>Contacts</li>
         </ul>
         <div className="nav-connect">Connect with Me</div>
     </div>
